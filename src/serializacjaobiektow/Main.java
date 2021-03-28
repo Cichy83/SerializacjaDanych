fbfd599f475da6480aec75984c4d88e3c562b3da
bartoszp <bartoszpypec@poczta.onet.pl>
2021-03-29 00:43:11 +0200

    Final project.

--- src/serializacjaobiektow/Main.java
+++ src/serializacjaobiektow/Main.java
@@ -8,36 +8,29 @@ public class Main {
         Towar[] towar = new Towar[3];
         towar[0] = new Towar();
         towar[1] = new Towar(29.0, "VideoKurs Java");
-        towar[2] = new Towar(8.2, "VideoKurs C++", 2008,11,21);
+        towar[2] = new Towar(8.2, "VideoKurs C++", 2008, 11, 21);
 
-        try{
-            RandomAccessFile RAF = new RandomAccessFile("baza.txt", "rw");
-            Towar.zapiszDoPliku(towar,RAF);
+        try {
+            ObjectOutputStream outS = new ObjectOutputStream(new FileOutputStream("baza.txt"));
+            outS.writeObject(towar);
 
-            RAF.seek(0);
 
-            Towar[] a = Towar.odczytajZPliku(RAF);
+            outS.close();
 
-            for(int i = 0; i < a.length; i++){
-                System.out.println(a[i].getCena());
-                System.out.println(a[i].getNazwa());
-                System.out.println(a[i].getDataWydania());
-                System.out.println("---------------------");
+            ObjectInputStream inS = new ObjectInputStream(new FileInputStream("baza.txt"));
+
+            Towar[] a = (Towar[]) inS.readObject();
+            for (int i = 0; i < a.length; i++) {
+                System.out.println(a[i]);
             }
 
-            try {
-                Towar b = new Towar();
-                b.czytajRekord(RAF, 3);
-                System.out.println(b);
-            }
-            catch(BrakRekordu e) {
-                System.out.println(e.getMessage());
-            }
 
-            RAF.close();
-            }
-        catch (IOException e){
+            inS.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
 }
+
--- src/serializacjaobiektow/Towar.java
+++ src/serializacjaobiektow/Towar.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class Towar {
+public class Towar implements  Serializable {
 
     public static final int DLUGOSC_NAZWY = 30;
     public static final int DLUGOSC_REKORDU = (Character.SIZE * DLUGOSC_NAZWY + Double.SIZE + 3 * Integer.SIZE) / 8;
@@ -13,6 +13,13 @@ public class Towar {
     private double cena; //8 bajtów
     private String nazwa; // DLUGOSC_NAZWY * 2 bajtów
     private Date dataWydania;
+
+    public String getHaslo() {
+        return this.haslo;
+    }
+
+    private transient String haslo = "koza";
+
 
     public Towar() {
         this.cena = 0.0;
@@ -64,67 +71,13 @@ public class Towar {
                 (kalendarz.get(Calendar.MONTH)+1) + " miesiąc, " + kalendarz.get(Calendar.DAY_OF_MONTH) + " dzień";
     }
 
-    public static void zapiszDoPliku(Towar[] towar, DataOutput outS) throws IOException {
-        for (int i = 0; i < towar.length; i++){
-            towar[i].zapiszDane(outS);
-        }
-    }
-
-    public static Towar[] odczytajZPliku(RandomAccessFile RAF) throws IOException {
-        int ileRekordow = (int)RAF.length()/DLUGOSC_REKORDU;
-        Towar[] towar = new Towar[ileRekordow];
-
-        for (int i = 0; i < ileRekordow; i++){
-            towar[i] = new Towar();
-            towar[i].czytajDane(RAF);
-        }
-    return towar;
-    }
-
-    public void zapiszDane (DataOutput outS) throws IOException {
-        outS.writeDouble(this.cena);
-
-        StringBuffer stringB = new StringBuffer(DLUGOSC_NAZWY);
-        stringB.append(this.nazwa);
-        stringB.setLength(DLUGOSC_NAZWY);
-        outS.writeChars(stringB.toString());
-
-        GregorianCalendar kalendarz = new GregorianCalendar();
-        kalendarz.setTime(this.dataWydania);
-
-        outS.writeInt(kalendarz.get(Calendar.YEAR));
-        outS.writeInt(kalendarz.get(Calendar.MONTH)+1);
-        outS.writeInt(kalendarz.get(Calendar.DAY_OF_MONTH));
-    }
-
-    public void czytajDane(DataInput inS) throws IOException {
-        this.cena = inS.readDouble();
-
-        StringBuffer tString = new StringBuffer(Towar.DLUGOSC_NAZWY);
-        for(int i = 0; i < Towar.DLUGOSC_NAZWY; i++){
-            char tCh = inS.readChar();
-            if (tCh != '\0'){
-                tString.append(tCh);
+    private void readObject (ObjectInputStream inS) throws IOException, ClassNotFoundException{
+        inS.defaultReadObject();
+        if (haslo != null){
+            if (!haslo.equals("tajne")){
+                throw new IOException("Dane są nieprawidłowe");
             }
         }
-        this.nazwa = tString.toString();
 
-        int rok = inS.readInt();
-        int m   = inS.readInt();
-        int dz  = inS.readInt();
-
-        GregorianCalendar kalendarz = new GregorianCalendar(rok, m-1, dz);
-        this.dataWydania = kalendarz.getTime();
     }
-
-    public void czytajRekord( RandomAccessFile RAF, int n) throws IOException, BrakRekordu {
-        if (n <= RAF.length() / Towar.DLUGOSC_REKORDU ) {
-            RAF.seek((n-1)*Towar.DLUGOSC_REKORDU);
-            this.czytajDane(RAF);
-        }
-        else {
-            throw new BrakRekordu("Niestety nie ma takego rekordu");
-        }
-    }
-
 }
